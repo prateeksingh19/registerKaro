c5d7ef925e98ee7e12c00896d66c035438e869c2
Prateek Singh <prateekvns99@gmail.com>
2024-06-26 21:14:59 +0530

    add ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,21 +1,23 @@
 module.exports = {
-  apps : [{
-    script: 'npm start',
-  }
-    ],
+  apps: [
+    {
+      script: "npm start",
+    },
+  ],
 
-  deploy : {
-    production : {
-      key:'key2.pem',
-      user : 'ubuntu',
-      host : '51.20.118.116',
-      ref  : 'origin/main',
-      repo : 'https://github.com/dev-1ce/RegisterKaro.git',
-      path : '/home/ubuntu',
-      'pre-deploy-local': '',
-      'post-deploy' : 'source ~/.nvm/nvm.sh &&  npm install && npm run build && pm2 reload ecosystem.config.js --env production',
-      'pre-setup': '',
-      'ssh_options':"ForwardAgent=yes"
-    }
-  }
+  deploy: {
+    production: {
+      key: "key.pem",
+      user: "ubuntu",
+      host: "13.127.222.194",
+      ref: "origin/main",
+      repo: "git@github.com:prateeksingh19/registerKaro.git",
+      path: "/home/ubuntu",
+      "pre-deploy-local": "",
+      "post-deploy":
+        "source ~/.nvm/nvm.sh && npm install && npm run build && pm2 reload ecosystem.config.js --env production",
+      "pre-setup": "",
+      "ssh-options": "ForwardAgent=yes",
+    },
+  },
 };
